11c7795c2555cc0a660f07838f2763ab5b4a2029
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2021-09-05 22:47:26 -0300

    Prototipo

---
+++ Jogo da velha/jogo_velha.js
@@ -0,0 +1,25 @@
+var jogador1 = document.getElementById("play1")
+var jogador2 = document.getElementById("play2")
+var men = document.getElementById("velha")
+var res = document.getElementById("prim")
+/*var res2 = document.getElementById("segu")
+var res3 = document.getElementById("terc")
+var res4 = document.getElementById("quar")
+var res5 = document.getElementById("quin")
+var res6 = document.getElementById("sest")
+var res7 = document.getElementById("sete")
+var res8 = document.getElementById("oito")
+var res9 = document.getElementById("nono")
+*/
+var i = 1
+
+function start(){
+    if(jogador1.value.length ==0||jogador2.value.length==" 0"){
+        alert("[ERRO] Falta jogadores")
+    }
+}
+while(i!=0){
+    men.innerHTML = "ola"
+}
+
+
