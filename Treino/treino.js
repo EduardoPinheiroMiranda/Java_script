8a41fb62ae59b0d0f02cafb77190b3ae31504448
EduardoPinheiroMiranda <pinheiomiranda@gmail.com>
2021-09-16 22:33:59 -0300

    ta dificil

--- Treino/treino.js
+++ Treino/treino.js
@@ -4,7 +4,7 @@ var analize= document.getElementById("analise")
 var banco=[]
 
 function tem_numero(n){
-    if(Number(n)>=0 && Number(n)<=100){
+    if(Number(n)==0){
         return true
     }else{
         return false
@@ -18,60 +18,14 @@ function tem_lista(n,l){
     }
 }
 function confirmar(){
-    if(tem_numero(valor.value) && !tem_lista(valor.value, banco)){
-        alert("ok")
-    }else{
-        alert("errado")
-    }
+      
+    if(tem_numero(valor.value) || tem_lista(valor.value, banco)){
+            alert("[ERRO] Verifique se você digitou um valor, ou se o valor digitado está sendo repetido! ")
+        }else{
+            banco.push(valor.value)
+            var item = document.createElement('option')
+            item.text=valor.value
+            exibir.appendChild(item)
+            analize.innerHTML=banco
+        }
 }
-/*
-function confirmar(){
-    if(valor.value.length==0){
-        alert("[ERRO] Você não digitou nenhum valor!")
-    }else{
-        banco.push(valor.value)
-        var item=document.createElement('option')
-        var aux=banco.length
-            if(aux>1){
-                for(var i=0;i<aux-1;i++){
-                    if(banco[i]==valor.value){
-                        alert("[ERRO] Você ja digitou esse valor!")
-                    }else{
-                        item.text=valor.value
-                        exibir.appendChild(item)
-                    }
-                }
-            }else{
-                item.text=valor.value
-                exibir.appendChild(item)
-            }
-            
-
-        
-    }
-}
-
-function fim(){
-    if(valor.value.length==0){
-        alert("[ERRO] Você não digitou nenhum valor!")
-    }else{
-        var aux
-        aux=banco.length
-        analize.innerHTML+=`Ao todo temos ${aux} valores<p>`
-        aux=0
-            for(var i=0 in banco){
-                if(banco[i]>aux){
-                    aux=banco[i]
-                }
-            }
-            analize.innerHTML+=`O maior número é ${aux}<p>`
-            aux=9999999
-            for(var i=0 in banco){
-                if(banco[i]<aux){
-                    aux=banco[i]
-                }
-            }
-            analize.innerHTML+=`O menor número é ${aux}<p>`
-            
-    }
-}*/
